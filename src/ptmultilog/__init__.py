3c00b90043a398b27fe02386aafc720ff56be71b
David Pochet <david.pochet@gmail.com>
2020-04-25 18:47:48 +0200

    Better API, and add requirements

--- src/ptmultilog/__init__.py
+++
@@ -1,2 +0,0 @@
-# pytest-multilog API
-from ptmultilog.helper import TestHelper  # noqa: F401
--- src/tests/test_simple.py
+++ src/tests/test_simple.py
@@ -1,8 +1,14 @@
 # Test for Test Helper
-from ptmultilog import TestHelper
+from pathlib import Path
+
+from pytest_multilog import TestHelper
 
 
 class TestTheHelper(TestHelper):
     def test_simple(self):
-        # Well, nothing at the moment
-        pass
+        # Just some basic tests
+        assert self.worker == "gw0"
+        assert self.root_folder == Path(__file__).parent.parent.parent
+        assert self.output_folder == self.root_folder / "out" / "tests"
+        assert self.test_folder == self.output_folder / "__running__" / "gw0" / "test_simple_TestTheHelper_test_simple"
+        assert (self.test_folder / "pytest.log").exists()
