1eb08bce50d9021308535a6d26094f5a63c0c4c6
David Pochet <david.pochet@gmail.com>
2020-11-15 15:27:34 +0100

    Add worker and logs related attributes
    * Add worker_index attribute
    * Add test_logs attribute
    * Add check_logs method
    * Updated documentation

--- src/tests/test_simple.py
+++ src/tests/test_simple.py
@@ -8,7 +8,10 @@ class TestTheHelper(TestHelper):
     def test_simple(self):
         # Just some basic tests
         assert self.worker == "gw0"
+        assert self.worker_index == 0
         assert self.root_folder == Path(__file__).parent.parent.parent
         assert self.output_folder == self.root_folder / "out" / "tests"
         assert self.test_folder == self.output_folder / "__running__" / "gw0" / "test_simple_TestTheHelper_test_simple"
-        assert (self.test_folder / "pytest.log").exists()
+        assert self.test_logs.exists()
+        self.check_logs("New test: test_simple_TestTheHelper_test_simple")
+        self.check_logs(["helper.py", "TestTheHelper"])
